ade0c2a3210ca6aaf81a7aa32b7c16b1c13079ef
harshith27k <104052246+harshith27kk@users.noreply.github.com>
2026-04-13 21:29:25 +0530

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> numbers = new HashMap<>();
+
+        for(int i = 0; i< nums.length; i++){
+            int diff = target - nums[i];
+            if(numbers.containsKey(diff)){
+                return new int[]{numbers.get(diff),i};
+            }
+            numbers.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}
