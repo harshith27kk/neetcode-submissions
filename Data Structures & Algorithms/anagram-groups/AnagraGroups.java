52c40b38c65542665598967092a07de94bff022a
harshith27kk <harshithh348@gmail.com>
2026-04-25 22:31:48 +0530

    debugging program

--- Data Structures & Algorithms/anagram-groups/AnagraGroups.java
+++ Data Structures & Algorithms/anagram-groups/AnagraGroups.java
@@ -1,4 +1,10 @@
-class Solution {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AnagraGroups {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> res = new HashMap<>();
 
@@ -11,5 +17,11 @@ class Solution {
             res.get(resultStr).add(s);
         }
         return new ArrayList<>(res.values());
+    }
+    public static void main(String[] args) {
+        AnagraGroups anagram = new AnagraGroups();
+        String[] str = {"act","pots","tops","cat","stop","hat"};
+        System.out.println(anagram.groupAnagrams(str));
+    
     }
 }
