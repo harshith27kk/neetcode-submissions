201562899bc557a756d3c6619c0bdd37995f779e
harshith27k <harshithh348@gmail.com>
2026-06-18 23:07:38 +0530

    Add: is-anagram - submission-12

---
+++ Data Structures & Algorithms/is-anagram/submission-12.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] sortedS = s.toCharArray();
+        Arrays.sort(sortedS);
+        char[] sortedT = t.toCharArray();
+        Arrays.sort(sortedT);
+
+        return Arrays.equals(sortedS,sortedT);
+
+    }
+}
