e4aa16b9e5488f72cb4e361ad4932eec3d80288c
harshith27k <harshithh348@gmail.com>
2026-06-18 23:18:09 +0530

    Add: is-anagram - submission-14

---
+++ Data Structures & Algorithms/is-anagram/submission-14.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        // char[] sortedS = s.toCharArray();
+        // Arrays.sort(sortedS);
+        // char[] sortedT = t.toCharArray();
+        // Arrays.sort(sortedT);
+
+        // return Arrays.equals(sortedS,sortedT);
+
+        // java streams
+        //  String sortedS = s.chars()
+        //  .sorted()
+        //  .mapToObj(c -> String.valueOf((char) c))
+        //  .collect(Collectors.joining());
+
+        // String sortedT = t.chars()
+        // .sorted()
+        // .mapToObj(c -> String.valueOf((char) c))
+        // .collect(Collectors.joining());
+
+        // return sortedS.equals(sortedT);
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+        for (int i : count) {
+            if (i != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
